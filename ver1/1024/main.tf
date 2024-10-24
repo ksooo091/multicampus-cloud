dabc1d95b08eede806e68330eabde8aba2eccdfa
KimGilSu <hwanrim00@gmail.com>
2024-10-24 22:46:56 +0900

    [1024] tg - health check path

--- ver1/1024/main.tf
+++ ver1/1024/main.tf
@@ -232,6 +232,9 @@ resource "aws_lb_target_group" "lab-tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.lab-vpc.id
+  health_check {
+    path = "/"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "tg-att" {
